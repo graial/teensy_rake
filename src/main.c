91ceb49e58189c135cdcee0414dbb63176424640
graial <allan.gray@sunEtrike.com>
2021-12-18 20:27:27 +0800

    refactor main into c file

---
+++ src/main.c
@@ -0,0 +1,18 @@
+#include "LedController.h"
+#include "LedScheduler.h"
+
+int main(void)
+{
+  uint16_t activeLeds;
+  LedController_Create(&activeLeds);
+  LedScheduler_Create();
+
+  int boardLed = 13;
+  int delay = 500;
+  LedController_Activate(boardLed);
+  LedScheduler_ScheduleDelay(boardLed, delay);
+
+  while (1) {
+    LedScheduler_WakeUp();
+  }
+}
