6733c1b132183962b4ac081ddd9851b91151b789
graial <allan.gray@sunEtrike.com>
2021-12-18 19:19:23 +0800

    refactor main

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,20 @@
 extern "C" {
   #include "LedController.h"
   #include "LedScheduler.h"
-}
 
-extern "C" int main(void)
-{
-uint16_t activeLeds;
-    LedController_Create(&activeLeds);
-    LedScheduler_Create();
+  int main(void)
+  {
+  uint16_t activeLeds;
+      LedController_Create(&activeLeds);
+      LedScheduler_Create();
 
-    int boardLed = 13;
-    int delay = 500;
-    LedController_Activate(boardLed);
-    LedScheduler_ScheduleDelay(boardLed, delay);
+      int boardLed = 13;
+      int delay = 500;
+      LedController_Activate(boardLed);
+      LedScheduler_ScheduleDelay(boardLed, delay);
 
-  while (1) {
-    LedScheduler_WakeUp();
+    while (1) {
+      LedScheduler_WakeUp();
+    }
   }
 }
