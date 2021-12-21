25e08af4761953f238bf0753c6b0f132e9ccfdc9
graial <allan.gray@sunEtrike.com>
2021-12-21 21:44:41 +0800

    add original main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,9 @@
 #include <Arduino.h>
 
-extern "C" {
-	#include "LedController.h"
-}
-
 extern "C" int main(void)
 {
 uint16_t activeLeds;
-    LedController_Create(&activeLeds);
-    int boardLed = 13;
-    LedController_Activate(boardLed);
+    pinMode(13, OUTPUT);
     
 	while (1) {
 		digitalWriteFast(13, HIGH);
